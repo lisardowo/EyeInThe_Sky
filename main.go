337e8e3a2165a70d7903990a2fd15038c6b39ef1
Lisardowo <lisardowo@gmail.com>
2026-05-01 12:36:14 -0600

    test telemetry fram

--- main.go
+++ main.go
@@ -1,1 +1,34 @@
-s
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type TrustLevel int
+
+const (
+	
+	Secure TrustLevel = iota //iota is a number generator -> this will be 0
+	Unsecure // and this 1 (kind of stupid tho)
+
+)
+
+type TelemetryFrame struct {
+	SourceID string
+	Level	TrustLevel
+	Data	string
+	Latency	time.Duration
+}
+
+func main(){
+	labFrame := TelemetryFrame{
+		SourceID: "CompletelyRealTestFrame",
+		Level: Unsecure,
+		Data: "inbound connection detected on sum port ",
+		Latency: time.Millisecond * 15,
+	}
+
+	fmt.Printf("---EYE IN THE SKY --- \n")
+	fmt.Printf("Source: %s\n DATA: %s\n", labFrame.SourceID, labFrame.Data)
+}
