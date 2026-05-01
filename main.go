f1fec3aa4c5077765faf501f12005b83c636eab4
Lisardowo <lisardowo@gmail.com>
2026-05-01 13:03:29 -0600

    simulation of unsecure and secure sources

--- main.go
+++ main.go
@@ -21,8 +21,14 @@ type TelemetryFrame struct {
 	Latency	time.Duration
 }
 
+var analysisMode TrustLevel = Unsecure //TODO harcorded value for testing only, the analysisMode restrains the capabilities
+//of communication, in a secure source, communication can be lighter, in a unsecure communication is way more restricted, besides other UI/UX changes
+//Analysis Mode is supposed to be obtained via a "handshake" when both devices are connected, before anything starts
+
 func main(){
-	labFrame := TelemetryFrame{
+
+
+	currentLabFrame := TelemetryFrame{
 		SourceID: "CompletelyRealTestFrame",
 		Level: Unsecure,
 		Data: "inbound connection detected on sum port ",
@@ -30,5 +36,28 @@ func main(){
 	}
 
 	fmt.Printf("---EYE IN THE SKY --- \n")
-	fmt.Printf("Source: %s\n DATA: %s\n", labFrame.SourceID, labFrame.Data)
+	fmt.Printf("Source: %s\n DATA: %s\n", currentLabFrame.SourceID, currentLabFrame.Data)
+	if analysisMode == Unsecure{
+		fmt.Println("Analyzing payload from an untrusted source")
+		
+		input := []TelemetryFrame{currentLabFrame}
+		unsecurePayloads := filterUnsecure(input)
+		fmt.Printf("Filtered Frames Count: %d\n", len(unsecurePayloads))
+
+	}else if analysisMode == Secure{
+		fmt.Println("Analyzing payload from a trusted source")
+	}
+	
+}
+
+func filterUnsecure(frames []TelemetryFrame) []TelemetryFrame{
+	
+	var unsecureFrame []TelemetryFrame
+
+	for _,f := range frames{
+		if f.Level == Unsecure {
+			unsecureFrame = append(unsecureFrame,f)
+		}
+	}
+	return unsecureFrame
 }
