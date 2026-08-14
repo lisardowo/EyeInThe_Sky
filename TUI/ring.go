4f9765607d6f9bbacc795283204050e2675906de
Lisardowo <lisardowo@gmail.com>
2026-08-14 09:04:07 -0600

    Implemented get method for ring buffer

--- TUI/ring.go
+++ TUI/ring.go
@@ -7,7 +7,7 @@ type RingBuffer[Data any] struct {
 	Size 	int
 	//muteX 	sync.Mutex
 	Head	int
-	Count	int
+	Position	int
 }
 
 func NewBuffer[Data any](size int) *RingBuffer[Data] {
@@ -17,19 +17,31 @@ func NewBuffer[Data any](size int) *RingBuffer[Data] {
 	})
 }
 
-func (ringBuff *RingBuffer[Data]) Add (logEntry Data)(int){
+func (ringBuff *RingBuffer[Data]) Add (logEntry Data){
 	
 	//ringBuff.muteX.Lock()
 	//defer ringBuff.muteX.Unlock() // Locks the buffer to prevent race conditions
 
 	ringBuff.Buffer[ringBuff.Head] = logEntry // Appends the log entry to the head
-	//ringBuff.Head = (ringBuff.Head + 1) % ringBuff.Size // Resets the index back to zero when .head == .size
+	ringBuff.Head = (ringBuff.Head + 1) % ringBuff.Size // Resets the index back to zero when .head == .size
 
-	if ringBuff.Count < ringBuff.Size{
-		ringBuff.Count++
+	if ringBuff.Position < ringBuff.Size{
+		ringBuff.Position++
 	}
 
-	return 0 //
+
+}
+
+func  (ringBuff *RingBuffer[Data]) GetEntries() []Data{
+	
+	result:= make([]Data, 0, ringBuff.Size)
+
+	for i := 0 ; i < ringBuff.Position ; i ++ {
+		index := (ringBuff.Head + ringBuff.Size - ringBuff.Position + i) % ringBuff.Size
+		result = append(result, ringBuff.Buffer[index])
+	}
+
+	return result
 
 }
 
